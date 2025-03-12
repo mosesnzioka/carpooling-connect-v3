31a134231b48ddfd6333a26c5a64b62ff6316276
mosesnzioka <mosesnzioka1@gmail.com>
2025-03-12 08:27:02 +0300

    Fix React Router deployment issue on Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,29 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
 
+// import { defineConfig } from "vite";
+
+// import tailwindcss from "@tailwindcss/vite";
 
 // export default defineConfig({
-//   plugins: [react()],
+//   esbuild: {
+//     jsxInject: `import React from 'react'`
+//   },
+//   plugins: [
+//     tailwindcss(),
+//   ],
 // });
 
 
-
-
 import { defineConfig } from "vite";
-
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  esbuild: {
-    jsxInject: `import React from 'react'`
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
   },
-  plugins: [
-    tailwindcss(),
-  ],
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
 });
